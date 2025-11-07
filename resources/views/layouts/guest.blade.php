c4451e728d71a60dffb75e85225df4cec41d6490
Ac25 <itisjs2003@gmail.com>
2025-11-07 14:35:30 +0800

    review

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,8 +12,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        {{-- Temporarily disabled for testing --}}
-        {{-- <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}"></script> --}}
+        <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}" async defer></script>
         
         <!-- Debug script -->
         <script>
